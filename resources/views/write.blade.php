8a46b9c2f665c9a541eab016221157286e098bf7
Adel Alawami <104333535+Adel1Alawami@users.noreply.github.com>
2023-01-17 15:31:49 +0200

    Update write.blade.php

--- resources/views/write.blade.php
+++ resources/views/write.blade.php
@@ -4,6 +4,7 @@
 
 
 <head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" class="jsx-5be976455d976918">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
     <title>AI CHATGPT-3</title><meta name="next-head-count" content="4"><link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     
