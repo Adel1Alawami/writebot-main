41599fbe0d23c201cbaea7a15bf19941370b6a0c
Adel1Alawami <104333535+Adel1Alawami@users.noreply.github.com>
2023-06-24 02:04:30 +0200

    Update write.blade.php

--- resources/views/write.blade.php
+++ resources/views/write.blade.php
@@ -85,24 +85,48 @@
 
 
 
-</main></div><div class="dark hidden bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-[260px] md:flex-col"><div class="flex h-full min-h-0 flex-col "><div class="scrollbar-trigger flex h-full w-full flex-1 items-start border-white/20"><nav class="flex h-full flex-1 flex-col space-y-1 p-2">
-    <a href="/" class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm mb-2 
-    flex-shrink-0 border border-white/20"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" 
-    stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="5" x2="12" y2="19">
+</main></div><div class="dark hidden bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-[260px] md:flex-col"><div class="flex h-full min-h-0 flex-col "><div class="scrollbar-trigger flex h-full w-full flex-1 items-start border-white/20">
+    
+    <nav class="flex h-full flex-1 flex-col space-y-1 p-2">
+        <a href="https://writebot-main-production.up.railway.app" class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm mb-2 
+        flex-shrink-0 border border-white/20"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="5" x2="12" y2="19">
+            
+        </line><line x1="5" y1="12" x2="19" y2="12"></line></svg>Home</a>
+    
+        <a href="/" class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 
+     transition-colors duration-200 text-white cursor-pointer text-sm"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2
+      0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
+    
+    </svg>Ask a Relative </a>
+       <a href="https://writebot-main-production.up.railway.app/write" class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
+        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 
+         0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line>
+        </svg>Clear conversations</a><a href="https://discord.gg/openai" target="_blank" class="bi-discord flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10
+     transition-colors duration-200 text-white cursor-pointer text-sm">OpenAI Discord</a>
+     <a href="https://beta.openai.com/docs/guides/completion" target="_blank" class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 
+     transition-colors duration-200 text-white cursor-pointer text-sm"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2
+      0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
+    
+    </svg>Prompt &amp; Structuring</a>
+    
+    
+    
+    
+      <a hidden id="btnFull" class="bi-fullscreen flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
+        Fullscreen</a>
+    
+        <img src="" alt="" id="image_ff">
         
-    </line><line x1="5" y1="12" x2="19" y2="12"></line></svg>Home</a>
-   <a href="/write" class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
-    <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4"
-     height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 
-     0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line>
-    </svg>Clear conversations</a><a href="https://discord.gg/openai" target="_blank" class="bi-discord flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10
- transition-colors duration-200 text-white cursor-pointer text-sm">OpenAI Discord</a>
- <a href="https://beta.openai.com/docs/guides/completion" target="_blank" class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 
- transition-colors duration-200 text-white cursor-pointer text-sm"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" 
- stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2
-  0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>Prompt &amp; Structuring</a>
-  <a id="btnFull" class="hidden bi-fullscreen flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
-    Fullscreen</a></nav></div></div></div></div></div><script id="__NEXT_DATA__" type="application/json"></script>
+        <polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
+       
+         
+    
+    
+    
+    </nav>
+
+
+</div></div></div></div></div><script id="__NEXT_DATA__" type="application/json"></script>
     <iframe height="1" width="1" style="position: absolute; top: 0px; left: 0px; border: none; visibility: hidden;"></iframe><next-route-announcer><p aria-live="assertive" id="__next-route-announcer__" role="alert" style="border: 0px; clip: rect(0px, 0px, 0px, 0px); height: 1px; margin: -1px; overflow: hidden; padding: 0px; position: absolute; width: 1px; white-space: nowrap; overflow-wrap: normal;"></p></next-route-announcer>
 
 
